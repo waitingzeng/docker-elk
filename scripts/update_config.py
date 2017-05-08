8497effdeb92242b50ceed60009b9682a4c1279a
waiting <ttwait@gmail.com>
2017-05-08 11:15:26 +0800

    update

--- scripts/update_config.py
+++ scripts/update_config.py
@@ -9,6 +9,8 @@ templates_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/
 
 
 for fname in os.path.dirname(templates_dir):
+    if not fname.endswith('.json'):
+        continue
     fname = templates_dir + '/' + fname
     content = json.loads(file(fname).read())
     requests.put('http://elastic:changeme@127.0.0.1:9200', json=content)
