d93ee65e3a6f5e4f86add52d0048b37cb6c135da
waiting <ttwait@gmail.com>
2017-05-08 11:19:43 +0800

    update

--- scripts/update_config.py
+++ scripts/update_config.py
@@ -8,7 +8,7 @@ import json
 templates_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/elasticsearch/templates'
 
 
-for basename in os.path.dirname(templates_dir):
+for basename in os.path.listdir(templates_dir):
     if not basename.endswith('.json'):
         continue
     fname = templates_dir + '/' + basename
